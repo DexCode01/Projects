77c566bdca537f6ae9744061633d0986e1e2d4a3
Dexadmin <dexcode14@gmail.com>
2026-05-08 16:50:48 -0700

    basic datatypes

---
+++ month1/datatype.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+  var a bool = true // a boolean
+  var b string = "Hello world" // a string 
+  var c int = 45 // an integer
+  var d float32 = 3.142 // a floating number
+
+  fmt.Println("boolean :", a)
+  fmt.Println("string :", b)
+  fmt.Println("integer :", c)
+  fmt.Println("float :", d)
+
+
+}
+
+  
